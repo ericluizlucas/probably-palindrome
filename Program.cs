4e35293cf4b981b15e4a208026809158b7ac7a9a
eric <eric@2upsolucoes.com.br>
2023-03-23 19:55:40 -0300

    Algoritmo palavras potencialmente palíndromas

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿class sc
+{
+    public char letter;
+    public int count;
+}
+
+class comp : Comparer<sc>
+{
+    public override int Compare(sc? x, sc? y)
+    {
+        return (x.letter.CompareTo(y.letter));
+    }
+}
+
+class Solver
+{
+    static bool Solve(string input)
+    {
+        if(input == "") return true;
+        bool resp = false;
+        char[] arr = input.ToCharArray();
+
+        sc[] element = new sc[arr.Length];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            element[i] = new sc();
+            element[i].letter = arr[i];
+            element[i].count = 0;
+        }
+
+
+
+        element[0].count = 1;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(i);
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (element[i].count != -1 && element[j].count != -1)
+                {
+                    if (element[i].letter == element[j].letter)
+                    {
+                        element[i].count += 1;
+                        element[j].count = -1;
+                    }
+                    else
+                    {
+                        element[j].count = 1;
+                    }
+                }
+
+            }
+
+
+        }
+
+        int qttOdd = 0;
+        for (int k = 0; k < element.Length; k++)
+        {
+
+            if (element[k].count != -1 && element[k].count % 2 != 0) qttOdd += 1;
+
+            Console.WriteLine(element[k].letter + " " + element[k].count);
+        }
+        Console.WriteLine(qttOdd);
+
+        if(qttOdd <= 1) resp = true;
+
+        return resp;
+    }
+
+    public static void Main()
+    {
+        string input = "abbac";
+        bool resp = Solve(input);
+
+        Console.WriteLine(resp);
+    }
+
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,21 @@
+Observe as seguintes strings:
+
+"parar"
+"temem"
+"technation"
+
+"parar" e "temem" são chamadas de palavras potencialmente palíndromas.
+
+Isso ocorre porque você pode rearranjar/permutar as letras dessas palavras de forma que elas se tornem palíndromas.
+
+"parar" => "rapar"
+"temem" => "metem"
+
+Enquanto isso "technation" nunca será uma palavra palíndroma, independentemente de como você reorganizar as letras dela.
+
+Observações adicionais:
+
+1) Não é necessário que as strings sejam palavras que existem na vida real.
+
+2) Considere a string vazia como potencialmente palíndroma.
+
